57c52007cd07c11283d7eacb3d73b71960bd75a6
Jaising <jaisinga@yahoo.com>
2015-02-22 16:20:10 -0600

    uploading cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,54 @@
-## Put comments here that give an overall description of what your
-## functions do
+# makeCacheMatrix - creates a "matrix" object which will then have its
+# matrix inverse computed using the solve() R function
 
-## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
+        m<-NULL
+        set<-function(y){
+                x<<-y
+                m<<-NULL
+        }
+        get<-function() x
+        
+        setmatrix<-function(solve) m<<- solve
+        
+        getmatrix<-function() m
+        
+        list(set=set, get=get, setmatrix=setmatrix, getmatrix=getmatrix)
+        
+# return the list of functions
 
 }
 
 
-## Write a short comment describing this function
+
+# cacheSolve - determines whether the need exists to compute
+# the matrix inverse via getting the value of the matrix inverse from
+# the makeCacheMatrix routine. If it is NULL, the matrix inverse will be
+# computed; if it is not NULL this implies that the matrix inverse was already
+# computed and is "cached"
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+        
+        m <- x$getmatrix()
+        #obtain the value of the matrix inverse as computed in makeCacheMatrix
+        
+        if(!is.null(m)){
+                message("getting cached data")
+                return(m)
+        # If the matrix inverse in not NULL, it means that it 
+        # has already been computed
+        # return the cached data and exit the routine
+        }
+        datos<-x$get()
+        # Get the matrix for which the inverse must be computed
+        
+        m<-solve(datos, ...)
+        
+        # Compute the matrix inverser using the "solve()" function
+        
+        x$setmatrix(m)
+        m
+        # return the newly computed matrix inverser
 }
